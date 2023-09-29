1d2aa8a545eed5440c8318aa2f76c75374bfcb9f
josheeen <therealjosh26@gmail.com>
2023-09-29 20:03:32 +0800

    Week 03 home page enhancement | Responsive Menu/Navigation

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,14 @@
 
 document.getElementById("year").innerHTML = (new Date).getFullYear();
 document.getElementById("lastModified").innerHTML = `Last Modified: ${new Date(document.lastModified)}`;
+
+
+// Hamburger Button Menu
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
