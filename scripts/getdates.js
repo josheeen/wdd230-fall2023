570b8d463b3916d97d895c99ad2e928f665e466d
josheeen <therealjosh26@gmail.com>
2023-11-14 13:45:55 +0800

    week 7 home page build enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -12,3 +12,14 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+if (typeof (Storage) !== "undefined") {
+    if (localStorage.visitCount) {
+        localStorage.visitCount = parseInt(localStorage.visitCount) + 1;
+    } else {
+        localStorage.visitCount = 1;
+    }
+    document.getElementById("visitCount").textContent = localStorage.visitCount;
+} else {
+    alert("Browser not suported");
+}
